4131822a8dc77ad6578e83226eb524b8d2c38dac
izebiafe <afimiafidelis95@gmail.com>
2023-11-10 12:10:07 +0100

    initialize classroom and add attributes

--- Clsasses/person.rb
+++ Clsasses/person.rb
@@ -1,29 +1,28 @@
 require_relative 'nameable'
 
 class Person < Nameable
+  attr_accessor :name, :age, :rentals
+  attr_reader :id
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
     @name = name
-    @age = age
+     @age = age.to_i
     @parent_permission = parent_permission
+    @rentals = []
   end
-  attr_accessor :name, :age
-  attr_reader :id
 
   def correct_name
     @name
   end
-
   private
-
   def of_age?
     @age >= 18
   end
-
   public
-
   def can_use_services?
     @age >= 18 || @parent_permission
   end
+
 end
